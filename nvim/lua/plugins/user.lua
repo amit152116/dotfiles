c191109835eaf90836b9b2efabcf28844ffba071
Amit152116Kumar <amit170103004@iitg.ac.in>
2025-06-10 22:00:01 +0530

    Auto-commit: pop-os sync at 10:00 PM by amit_152116 (Updated 1 files)
    Affected files:
     M nvim/lua/plugins/user.lua

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -48,10 +48,30 @@ return {
             require("CopilotChat").setup {
                 model = "claude-3.7-sonnet",
                 prompts = {
-                    MyCustomPrompt = {
-                        prompt = "Explain how it works.",
-                        system_prompt = "You are very good at explaining stuff",
-                        description = "My custom prompt description",
+                    Resuable = {
+                        prompt = "Extract reusable functions or methods from this code to improve modularity and testability.",
+                        system_prompt = "COPILOT_REVIEW", -- System prompt to use (can be specified manually in prompt via /).
+                        description = "Reusable prompt description",
+                    },
+                    Refactor = {
+                        prompt = "Refactor this code to improve readability and maintainability.",
+                        system_prompt = "COPILOT_REVIEW", -- System prompt to use (can be specified manually in prompt via /).
+                        description = "Refactor prompt description",
+                    },
+                    DailySummary = {
+                        prompt = "Summarize the key changes and contributions made today across all code files. Highlight major features, bug fixes, refactoring, and configuration updates. Keep the summary concise and suitable for inclusion in a daily work log or team stand-up note.",
+                        system_prompt = "COPILOT_INSTRUCTIONS", -- System prompt to use (can be specified manually in prompt via /).
+                        description = "Daily summary prompt description",
+                    },
+                    ROSRefactor = {
+                        prompt = "Refactor the selected code with ROS 2 best practices in mind. Preserve functionality but improve readability, modularity, and performance.",
+                        system_prompt = "COPILOT_REVIEW", -- System prompt to use (can be specified manually in prompt via /).
+                    },
+
+                    ROSReview = {
+                        prompt = "Review this ROS 2 C++ code for memory management, topic/service usage, and adherence to ROS 2 conventions.",
+                        system_prompt = "COPILOT_REVIEW",
+                        description = "ROS 2 code review prompt description",
                     },
                 },
             }
